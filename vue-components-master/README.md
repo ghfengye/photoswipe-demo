f93fae2bb07dd6b81de7bc0a315c7d79e5bfcbb0
马泽娜 <mazena@mazenadeMacBook-Pro.local>
2018-08-04 14:25:40 +0800

    点击查看大图的demo

---
+++ vue-components-master/README.md
@@ -0,0 +1,43 @@
+# vue-components-master
+
+> A Vue.js project
+
+## Build Setup
+
+```bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+# 封装可复用的组件
+
+# 点击图片放大图 photoswipe
+
+1.  安装
+
+```bash
+npm i photoswipe
+```
+
+2.  自定义插件 components/photoswipe
+3.  在 main.js 中全局注册
---
+++ vue-components-master/src/views/photoswipe.vue
@@ -0,0 +1,83 @@
+<template>
+   <div class="eye_image">
+      <template v-for="(item,index) in items">
+        <div class="eye-item" :key="index">
+          <div class="img-wrapper">
+            <img :src="item.src" @load="loaded($event, index)" class="preview-img" @click="$preview.open(index, previewList)">
+          </div>
+        </div>
+      </template>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      previewList: [],
+      items: [
+        {
+          src: "https://placekitten.com/600/400",
+          w: 600,
+          h: 400,
+          createTime: "2018.09.08",
+          eyeSide: 1
+        },
+        {
+          src: "https://placekitten.com/1200/900",
+          w: 1200,
+          h: 900,
+          createTime: "2018.09.08",
+          eyeSide: 1
+        },
+        {
+          src: "https://placekitten.com/800/400",
+          w: 1200,
+          h: 900,
+          createTime: "2018.09.08",
+          eyeSide: 1
+        },
+        {
+          src: "https://placekitten.com/1200/1200",
+          w: 1200,
+          h: 900,
+          createTime: "2018.09.08",
+          eyeSide: 1
+        }
+      ]
+    };
+  },
+  methods: {
+    loaded(event, index) {
+      var img = event.currentTarget;
+      this.previewList[index] = Object.assign(
+        {},
+        {
+          src: img.src,
+          w: img.naturalWidth,
+          h: img.naturalHeight
+        }
+      );
+    }
+  }
+};
+</script>
+<style  scoped>
+.eye_image {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+.eye-item {
+  width: 200px;
+  margin-top: 14px;
+  cursor: pointer;
+}
+img {
+  width: 100%;
+}
+.img-wrapper {
+  text-align: center;
+  background: #000;
+}
+</style>
